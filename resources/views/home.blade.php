bebdf025a6e8eb40ec00343597d8fd6c3d10d601
Marc Benedict Escatron <marc.escatron07@gmail.com>
2022-10-21 08:39:09 +0800

    updated style for dashboard panels

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,15 +7,15 @@
         </div>
         <div class="row mt-4 justify-content-center">
     
-           <div class="dashboard-panel col-3 d-flex flex-column justify-content-center align-items-center bg-light" style="height:200px;">
+           <div class="dashboard-panel col-3 d-flex flex-column justify-content-center align-items-center bg-light" style="height:180px;">
                 <h1 class="dashboard-title text-primary">Computers</h1>
                 <h2 class="mt-2">{{ $computerCount }}</h2>
             </div>    
-            <div class="dashboard-panel col-3 mx-5 d-flex flex-column justify-content-center align-items-center bg-light" style="height:200px;">
+            <div class="dashboard-panel col-3 mx-5 d-flex flex-column justify-content-center align-items-center bg-light" style="height:180px;">
                 <h1 class="dashboard-title text-danger">Desktops</h1>
                 <h2 class="mt-2">{{ $desktopCount }}</h2>
             </div>
-            <div class="dashboard-panel col-3 d-flex flex-column justify-content-center align-items-center bg-light" style="height:200px;">
+            <div class="dashboard-panel col-3 d-flex flex-column justify-content-center align-items-center bg-light" style="height:180px;">
                 <h1 class="dashboard-title text-success">Peripherals</h1>
                 <h2 class="mt-2">{{ $peripheralCount }}</h2>
             </div>
