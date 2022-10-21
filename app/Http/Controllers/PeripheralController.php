69ec27217faed62167262d86bd65f110ac698ff9
Marc Benedict Escatron <marc.escatron07@gmail.com>
2022-10-21 09:09:25 +0800

    applied old logic and commented out temporarily in PeripheralController

--- app/Http/Controllers/PeripheralController.php
+++ app/Http/Controllers/PeripheralController.php
@@ -93,27 +93,102 @@ class PeripheralController extends Controller
         //
     }
 
-    public function deleteAll() {
+    public function deleteAll($id, Request $request) {
+        // $verifyPassword = User::find($id)->password;
 
+        // if(Hash::check($request->delall_password, $verifyPassword)) {
+        //     DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        //     Peripheral::truncate();
+        //     DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
+        //     session()->flash('error','All data has been deleted!');
+        //     return redirect('/desktops');
+        // } else {
+        //     session()->flash('error','Entered password is incorrect!');
+        //     return back();
+        // }
     }
-    
-    public function showMonitor() {
 
+    public function showMonitor() {
+        // $typeID = 2;
+        // $logcount = MaintenanceLog::all();
+        // session(['logcount' => count($logcount)]);
+        
+        // $peripherals = Peripheral::where('type_id', '=', $typeID)->paginate(5);
+        // $statuses = Status::skip(0)->take(2)->get();
+        
+        // return view('pages.general.peripherals', compact('peripherals','statuses'));
     }
 
     public function showKeyboard() {
-
+        // $typeID = 3;
+        // $logcount = MaintenanceLog::all();
+        // session(['logcount' => count($logcount)]);
+        
+        // $peripherals = Peripheral::where('type_id', '=', $typeID)->paginate(5);
+        // $statuses = Status::skip(0)->take(2)->get();
+        
+        // return view('pages.general.peripherals', compact('peripherals','statuses'));
     }
 
     public function showMouse() {
-
+        // $typeID = 4;
+        // $logcount = MaintenanceLog::all();
+        // session(['logcount' => count($logcount)]);
+        
+        // $peripherals = Peripheral::where('type_id', '=', $typeID)->paginate(5);
+        // $statuses = Status::skip(0)->take(2)->get();
+        
+        // return view('pages.general.peripherals', compact('peripherals','statuses'));
     }
 
     public function showHeadset() {
-
+        // $typeID = 5;
+        // $logcount = MaintenanceLog::all();
+        // session(['logcount' => count($logcount)]);
+        
+        // $peripherals = Peripheral::where('type_id', '=', $typeID)->paginate(5);
+        // $statuses = Status::skip(0)->take(2)->get();
+        
+        // return view('pages.general.peripherals', compact('peripherals','statuses'));
     }
 
-    public function updateStatus() {
+    public function updateStatus(
+        // MaintenanceLog $maintenance, 
+        Request $request
+    ) {
+        // Peripheral::where('id', '=',$request->peripheralID)->update(['status_id' => $request->statusID]); 
 
+        // if($request->statusID == 2){
+        //     $peripheralQuery = Peripheral::find($request->peripheralID);
+        //     $peripheralUnique = strtoupper($peripheralQuery->serial_number);
+        //     $checkData = MaintenanceLog::where('serial_number', '=', $peripheralUnique)->first();
+
+        //     if($checkData) {
+        //         var_dump($checkData);
+        //     } else {
+        //         date_default_timezone_set('Asia/Manila');
+        //         $date = date('Y/m/d H:i:s');
+
+        //         $maintenance->assigned_computer = $peripheralQuery->computer->location->row.$peripheralQuery->computer->pc_number;
+        //         $maintenance->name = $peripheralQuery->name;
+        //         $maintenance->brand = $peripheralQuery->brand;
+        //         $maintenance->type = Type::find($peripheralQuery->type_id)->name;
+        //         $maintenance->serial_number = $peripheralUnique;     
+        //         $maintenance->logged_at = $date;                
+        //         $maintenance->remarks = "None";
+        //         $maintenance->save();
+        //     }
+        // } else {
+        //     $peripheralQuery = Peripheral::find($request->peripheralID);
+        //     $peripheralUnique = strtoupper($peripheralQuery->serial_number);
+
+        //     MaintenanceLog::where('serial_number', '=', $peripheralUnique)->delete();
+        //     MaintenanceLog::onlyTrashed()->where('serial_number', '=', $peripheralUnique)->first()->forceDelete();
+        //     DB::statement('ALTER TABLE maintenance_logs AUTO_INCREMENT=1;');
+        // }
+
+        // $mcount = count(MaintenanceLog::all());
+        // echo $mcount;
     }
 }
