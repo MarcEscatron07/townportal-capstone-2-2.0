cc062e8f0bb30045b4016727e36ea9aaf40e7c8a
Marc Benedict Escatron <marc.escatron07@gmail.com>
2022-10-21 19:25:42 +0800

    added comment to enable 'Forgot Your Password' feature in ForgotPasswordController

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -19,4 +19,21 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    /*
+        To enable 'Forgot Your Password' feature for Auth, do the following steps:
+        -   register in: https://mailtrap.io
+        -   go to Sandbox > Inboxes > Integrations > Select 'Laravel 7+'
+        -   copy the values and paste them into the .env file of the project
+            ex.
+                MAIL_MAILER=smtp
+                MAIL_HOST=smtp.mailtrap.io
+                MAIL_PORT=2525
+                MAIL_USERNAME=8fe9241b801557
+                MAIL_PASSWORD=2c63074963522a
+                MAIL_ENCRYPTION=tls
+        -   if the project is running through 'php artisan serve', you need to stop the project by CTRL + C
+        -   afterwards, in the terminal, run this command: php artisan config:cache
+        -   you can now run the project again and use the 'Forgot Your Password' feature.
+    */
 }
